54da03c44db70efbab3ad49967e6398b79181cb7
haciduru <59296085+haciduru@users.noreply.github.com>
2020-08-05 17:13:26 -0400

    Update main.r

--- main.r
+++ main.r
@@ -1,20 +1,12 @@
 
-    
   # init the world
-  G = init.w('...', 50)
+  G = init.w('/Users/haciduru/Downloads/dataverse_files/36-NY-cities-street_networks-node_edge_lists/3645073_Mannsville', 50)
   
-  plot(G, layout = layout_with_fr,  
-       vertex.label = '',
-       # vertex.label = substr(V(G)$name, nchar(V(G)$name) - 3, nchar(V(G)$name)),
-       vertex.label.cex = 1,
-       vertex.size = 1,
-       edge.arrow.size = .1, 
-       edge.color = 'black', 
-       edge.width = .5)
-  
-    
-  #init nodes
-  nodes = data.frame(name = row.names(as.matrix(V(G))), n_offender = 0, n_civilian = 0)
-  
+  #init nodes; nodes should be initialized before agents
+  nodes = init.nodes(G)
+
   # init agents
-  agents = init.agents(G, 10, .5, nodes)
+  agents = init.agents(G, 10, .2, nodes)
+  nodes = update.nodes(agents, nodes)
+    
+  for (j in 1:10) for (i in 1:length(agents)) { agents[[i]] = move.agent(agents[[i]], nodes); nodes = agent.attack(agents[[i]], nodes) }; nodes = update.nodes(agents, nodes); nodes[,3]; nodes[,4]; nodes[,5]; plot(G, layout = layout_with_fr, vertex.label = '', vertex.size = (nodes$n_victim + .1), edge.arrow.size = .1, edge.color = 'black', edge.width = .5)
